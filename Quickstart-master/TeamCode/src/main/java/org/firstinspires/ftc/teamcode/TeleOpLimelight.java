84d074ba0c4b31bdcb748850a0c8e46ddcaf3f65
ShreyanshVennamaneni <144498974+anshu7600@users.noreply.github.com>
2025-11-24 23:17:00 -0600

    Auton & Lemon & TeleOp

--- Quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLimelight.java
+++ Quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLimelight.java
@@ -1,25 +1,26 @@
 package org.firstinspires.ftc.teamcode;
-// import com.pedropathing.follower.Follower;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "TeleopBetter Work", group = "TeleOp")
-public class PedroTeleop extends OpMode {
+@TeleOp(name = "Lemon Teleop", group = "TeleOp")
+public class TeleOpLimelight extends OpMode {
     // private Follower follower;
     private DcMotorEx outtakeLeft, outtakeRight;
-    private DcMotor intake;
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    private DcMotor intake, frontLeft, frontRight, backLeft, backRight;
 
     double targetRPM = 2567;
     double targetRPMLow = 2067;
     private double ticksPerRev;
     private boolean rumbleTriggered = false;
+
+    private Limelight3A limelight;
+    private double lastError = 0;
 
     @Override
     public void init() {
@@ -47,15 +48,56 @@ public class PedroTeleop extends OpMode {
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
     }
 
     @Override
     public void start() {
         ticksPerRev = 28;
+        limelight.start();
     }
 
     @Override
     public void loop() {
+        controlOuttake();
+        controlIntake();
+        controlDrivetrain();
+        updateTelemetry();
+    }
+
+    public void updateTelemetry() {
+        telemetry.addData("Current target RPM ", targetRPM);
+        telemetry.addData("Left RPM ", outtakeLeft.getVelocity() * 60 / ticksPerRev);
+        telemetry.addData("Right RPM ", outtakeRight.getVelocity() * 60 / ticksPerRev);
+    }
+
+    private void controlDrivetrain() {
+        double y = -gamepad1.left_stick_y;  // Forward/backward
+        double x = gamepad1.left_stick_x * 1.1;  // Strafe
+        double rx = gamepad1.right_stick_x * .6;  // Rotation
+
+        if (gamepad1.b) {
+            rx = getLemonStuff();
+        }
+
+        double frontLeftPower = (y + x + rx) * .9;
+        double frontRightPower = (y - x - rx) * .9;
+        double backLeftPower = (y - x + rx) * .9;
+        double backRightPower = (y + x - rx) * .9;
+
+        SetDrivetrainMotorPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+    }
+
+    private void SetDrivetrainMotorPowers(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+    }
+
+    private void controlOuttake() {
         if (gamepad2.right_bumper) {
             double targetTicksPerSec = targetRPM * ticksPerRev / 60.0;
             outtakeLeft.setVelocity(-targetTicksPerSec);
@@ -68,7 +110,6 @@ public class PedroTeleop extends OpMode {
             outtakeLeft.setVelocity(0);
             outtakeRight.setVelocity(0);
         }
-
         double leftRPM = Math.abs(outtakeLeft.getVelocity() * 60 / ticksPerRev);
         double rightRPM = Math.abs(outtakeRight.getVelocity() * 60 / ticksPerRev);
         boolean ready = Math.abs(leftRPM - targetRPM) < 100 && Math.abs(rightRPM - targetRPM) < 100;
@@ -78,7 +119,9 @@ public class PedroTeleop extends OpMode {
         } else if (!ready) {
             rumbleTriggered = false;
         }
+    }
 
+    private void controlIntake() {
         if (gamepad2.left_bumper) {
             intake.setPower(.95);
         } else if (gamepad2.left_trigger > 0) {
@@ -86,54 +129,25 @@ public class PedroTeleop extends OpMode {
         } else {
             intake.setPower(0);
         }
+    }
 
-        if (gamepad1.right_bumper) {
-            targetRPM = targetRPM + 100;
-        } else if (gamepad1.left_bumper) {
-            targetRPM = targetRPM - 100;
+    private double getLemonStuff() {
+        LLResult results = limelight.getLatestResult();
+        boolean hasTarget = results != null && results.isValid();
+        if (!hasTarget) {
+            telemetry.addLine("No Tag Detected");
+            return 0;
         }
+        double yaw = results.getTx();  // positive = tag is right, negative = left
+        double derivative = yaw - lastError;
+        lastError = yaw;
+        double kP = 0.03;
+        double kD = 0.001;
+        double output = (kP * yaw) + (kD * derivative);
+        output = Math.max(-0.5, Math.min(0.5, output));
+        telemetry.addData("Tx", yaw);
+        telemetry.addData("TurnPower", output);
 
-//        follower.setTeleOpDrive(
-//                -gamepad1.left_stick_y,
-//                -gamepad1.left_stick_x,
-//                -gamepad1.right_stick_x,
-//                true);
-        controlDrivetrain();
-        updateTelemetry();
-    }
-
-    public void updateTelemetry() {
-        telemetry.addData("Current target RPM ", targetRPM);
-        telemetry.addData("Left RPM ", outtakeLeft.getVelocity() * 60 / ticksPerRev);
-        telemetry.addData("Right RPM ", outtakeRight.getVelocity() * 60 / ticksPerRev);
-    }
-
-    private void controlDrivetrain() {
-        if (gamepad1.dpad_up) SetDrivetrainMotorPowers(.9, .9, .9, .9);
-        else if (gamepad1.dpad_down) SetDrivetrainMotorPowers(-.9, -.9, -.9, -.9);
-        else if (gamepad1.dpad_left) SetDrivetrainMotorPowers(-.9, .9, .9, -.9);
-        else if (gamepad1.dpad_right) SetDrivetrainMotorPowers(.9, -.9, -.9, .9);
-        else {
-            // Default joystick control
-            double y = -gamepad1.left_stick_y;  // Forward/backward
-            double x = gamepad1.left_stick_x * 1.1;  // Strafe
-            double rx = gamepad1.right_stick_x * .6;  // Rotation
-
-
-            // different drive train
-            double frontLeftPower = (y + x + rx) * .9;
-            double frontRightPower = (y - x - rx) * .9;
-            double backLeftPower = (y - x + rx) * .9;
-            double backRightPower = (y + x - rx) * .9;
-
-            SetDrivetrainMotorPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
-        }
-    }
-
-    private void SetDrivetrainMotorPowers(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
-        frontLeft.setPower(frontLeftPower);
-        frontRight.setPower(frontRightPower);
-        backLeft.setPower(backLeftPower);
-        backRight.setPower(backRightPower);
+        return output;
     }
 }
